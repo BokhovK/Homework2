278a464d3b907337b694b2ce0ba022192c6ba2f8
kbohov <kbohov@mail.ru>
2024-02-15 19:17:46 +0300

    Переделал

--- src/Main.java
+++ src/Main.java
@@ -2,65 +2,93 @@ import java.security.spec.RSAOtherPrimeInfo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Задача 1");
         //Задача 1
-        for (int i = 1; i < 11; i++){
-            System.out.println(i);
+        System.out.println("Задача 1");
+        int money = 15_000;
+        int allMoney = 2_459_000;
+        double sum = 0;
+        int month = 0;
+        while (sum <= allMoney) {
+            month++;
+            sum += money;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + sum + " рублей");
+
         }
         //Задача 2
         System.out.println("Задача 2");
-        for (int i = 10; i > 0; i--) {
-            System.out.println(i);
+        int a = 0;
+        int b = 10;
+        while (a < b) {
+            a++;
+            System.out.print(" " + a + " ");
+        }
+        System.out.println();
+        for (int c = 10; c > 0; c--) {
+            System.out.print(" " + c + " ");
         }
         //Задача 3
         System.out.println("Задача 3");
-        for (int i = 0; i < 17; i = i+2) {
-            System.out.println(i);
+        int peoples = 12_000_000;
+        int burn = 17;
+        int die = 8;
+        int year = 10;
+        int yearNow = 2024;
+        for (int years = yearNow + 1; years <= yearNow + year; years++) {
+            peoples += (burn - die) * peoples / 1_000;
+            System.out.println("Год " + years + ", численность населения составляет" + peoples);
         }
         //Задача 4
         System.out.println("Задача 4");
-        for (int i = 10; i > -11; i--) {
-            System.out.println(i);
+        double percent = 0.07;
+        int deposit = 15000;
+        long summa = 12000000;
+        int months = 0;
+        while (deposit <= summa) {
+            months++;
+            deposit += deposit * percent;
+            System.out.println("Месяц " + months + ", сумма накоплений равна " + deposit + " рублей");
+
         }
         //Задача 5
         System.out.println("Задача 5");
-        for (int i = 1904; i < 2096; i = i+4) {
-            System.out.println(i + " год является високосным");
+        double percents = 0.07;
+        int deposits = 15000;
+        long summas = 12000000;
+        int monthss = 0;
+        while (deposits <= summas) {
+            monthss++;
+            deposits += deposits * percents;
+            if (monthss % 6 == 0) {
+                System.out.println("Месяц " + monthss + ", сумма накоплений равна " + deposits + " рублей");
+            }
         }
         //Задача 6
         System.out.println("Задача 6");
-        for (int i = 7; i < 105; i = i+7) {
-            System.out.println(i);
+        int mon = 12 * 9;
+        while (monthss < mon) {
+            monthss++;
+            deposits += deposits * percent;
+            if (monthss % 6 == 0) {
+                System.out.println("Месяц " + monthss + ", сумма накоплений равна " + deposits + " рублей");
+            }
         }
         //Задача 7
         System.out.println("Задача 7");
-        for (int i = 1; i < 1016; i = i*2) {
-            System.out.println(i);
+        int firstFriday = 7;
+        for (int friday = firstFriday; friday <= 31; friday += 7) {
+            System.out.println("Сегодня пятница, " +friday+ "-е число, нужно подготовить отчёт");
         }
         //Задача 8
         System.out.println("Задача 8");
-        int salary = 29000;
-        int total = 0;
-        for (int i = 0; i < 13; i++) {
-            total=total+salary;
-            System.out.println("Месяц " +i+ ", сумма накоплений равна " +total+" рублей");
-        }
-        //Задача 9
-        System.out.println("Задача 9");
-        int oneSalary = 29000;
-        int oneTotal = 0;
-        for (int i = 0; i < 13; i++) {
-            oneTotal = oneTotal + oneTotal / 100;
-            oneTotal = oneTotal + oneSalary;
-            System.out.println("Месяц " +i+ ", сумма накоплений равна " +oneTotal+" рублей");
-        }
-        //Задача 10
-        System.out.println("Задача 10");
-        int num=2;
-        for (int i = 1; i < 11; i++) {
-            System.out.println(num + "*" +i+"=" + num*i);
-        }
-
+        int mainYear=2024;
+        int startYear=mainYear-200;
+        int endYear=mainYear+100;
+        int period=79;
+        int startPeriod=0;
+        for (int oneYear = startYear; oneYear<=endYear; oneYear++) {
+            if (oneYear % period == startPeriod) {
+                System.out.println(oneYear);
+            }
     }
+  }
 }
