23d54f9d200fe5919cfb582f2035e5a8bbc7ef93
kbohov <kbohov@mail.ru>
2024-03-10 16:42:35 +0300

    Control Homework

---
+++ src/Employee.java
@@ -0,0 +1,64 @@
+import java.util.Objects;
+
+public class Employee {
+    private String fullName;
+    private int department;
+    private int salary;
+    private int id;
+
+    public static int idCounter = 0;
+
+    public Employee(String fullName, int department, int salary) {
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+        idCounter++;
+        id = idCounter;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return department == employee.department && salary == employee.salary && id == employee.id && Objects.equals(fullName, employee.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullName, department, salary, id);
+    }
+
+    @Override
+    public String toString() {
+        return "Сотрудник: " + fullName +
+                ", отдел: " + department +
+                ", зарплата: " + salary + " рублей" +
+                ", id: " + id;
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,50 @@
-import java.time.LocalDate;
-
 public class Main {
-
     public static void main(String[] args) {
-        Author blok = new Author("Александр", "Блок");
-        Author dostoevsky = new Author("Фёдор", "Достоевский");
+        DataBaseCompany dataBaseCompany = new DataBaseCompany();
+        dataBaseCompany.addEmployee("Иванов Александр Петрович", 3, 31_000);
+        dataBaseCompany.addEmployee("Алексеев Игорь Мстиславович", 1, 40_000);
+        dataBaseCompany.addEmployee("Антонов Алексей Георгиевич", 1, 41_000);
+        dataBaseCompany.addEmployee("Антипов Егор Николаевич", 3, 28_500);
+        dataBaseCompany.addEmployee("Васильев Платон Сергеевич", 2, 142_000);
+        dataBaseCompany.addEmployee("Сульянов Анатолий Султанович", 2, 112_000);
+        dataBaseCompany.addEmployee("Сахаров Захар Иннокентьевич", 5, 88_500);
+        dataBaseCompany.addEmployee("Землёв Никита Артемович", 4, 18_550);
+        dataBaseCompany.addEmployee("Вадиков Владимир Вадимович", 2, 109_520);
+        dataBaseCompany.addEmployee("Папазогло Сергей Петрович", 5, 23_770);
 
-        Book skif = new Book("Скиф", 1918, blok);
-        Book igrok = new Book("Игрок", 1866, dostoevsky);
 
-        print(skif);
-        print(igrok);
+        dataBaseCompany.printAllInformation();
+
+        dataBaseCompany.calculateSalary();
+
+        dataBaseCompany.findMinimalSalaryWorker();
+
+        dataBaseCompany.findHighestSalaryWorker();
+
+        dataBaseCompany.findAverageSalary();
+
+        dataBaseCompany.printAllEmployeesFullNames();
+
+        dataBaseCompany.calculateSalaryOfDepartment(1);
+
+        dataBaseCompany.findAverageSalaryOfDepartment(2);
+
+        dataBaseCompany.indexSalary();
+        dataBaseCompany.printAllInformation();
+
+        dataBaseCompany.findMinimalSalaryWorkerOfDepartment(3);
+
+        dataBaseCompany.findHighestSalaryWorkerOfDepartment(4);
+
+
+        dataBaseCompany.indexSalaryOfDepartment(10, 5);
+        dataBaseCompany.printAllInformation();
+
+        dataBaseCompany.printAllInformationOfDepartment(1);
+
+
+        dataBaseCompany.findWorkersWithSalaryBelowParameter(60_000);
+
+        dataBaseCompany.findWorkersWithSalaryHigherParameter(45_000);
     }
-
-    public static void print(Book book) {
-        System.out.println("Автор: " + book.getAuthor().getName() + " " + book.getAuthor().getSurname() +
-                        ", название: " + book.getTitle() + ", год издания: " + book.getYear());
-    }
-
 }
-
-
-
-
-
